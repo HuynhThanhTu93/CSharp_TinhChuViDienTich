0f38368d957b3624168df98d57716c121635a90e
HuynhThanhTu93 <tu0909493091@gmail.com>
2024-07-22 20:26:41 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CSharp_TinhChuViDienTich
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblA = new Label();
+            lblB = new Label();
+            txtB = new TextBox();
+            txtA = new TextBox();
+            btxCHUVI = new Button();
+            btxDIENTICH = new Button();
+            lbl = new Label();
+            button1 = new Button();
+            lblKQ = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // lblA
+            // 
+            lblA.AutoSize = true;
+            lblA.Location = new Point(57, 60);
+            lblA.Name = "lblA";
+            lblA.Size = new Size(97, 33);
+            lblA.TabIndex = 0;
+            lblA.Text = "Cạnh a";
+            // 
+            // lblB
+            // 
+            lblB.AutoSize = true;
+            lblB.Location = new Point(57, 121);
+            lblB.Name = "lblB";
+            lblB.Size = new Size(98, 33);
+            lblB.TabIndex = 0;
+            lblB.Text = "Cạnh b";
+            // 
+            // txtB
+            // 
+            txtB.Location = new Point(204, 114);
+            txtB.Name = "txtB";
+            txtB.Size = new Size(247, 40);
+            txtB.TabIndex = 2;
+            // 
+            // txtA
+            // 
+            txtA.Location = new Point(204, 53);
+            txtA.Name = "txtA";
+            txtA.Size = new Size(247, 40);
+            txtA.TabIndex = 1;
+            // 
+            // btxCHUVI
+            // 
+            btxCHUVI.Location = new Point(12, 315);
+            btxCHUVI.Name = "btxCHUVI";
+            btxCHUVI.Size = new Size(117, 47);
+            btxCHUVI.TabIndex = 2;
+            btxCHUVI.Text = "Chu Vi";
+            btxCHUVI.UseVisualStyleBackColor = true;
+            btxCHUVI.Click += btxCHUVI_Click;
+            // 
+            // btxDIENTICH
+            // 
+            btxDIENTICH.Location = new Point(151, 315);
+            btxDIENTICH.Name = "btxDIENTICH";
+            btxDIENTICH.Size = new Size(148, 47);
+            btxDIENTICH.TabIndex = 2;
+            btxDIENTICH.Text = "Diện Tích";
+            btxDIENTICH.UseVisualStyleBackColor = true;
+            btxDIENTICH.Click += btxDIENTICH_Click;
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Location = new Point(57, 187);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(110, 33);
+            lbl.TabIndex = 3;
+            lbl.Text = "Kết Quả";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 47);
+            button1.TabIndex = 2;
+            button1.Text = "TÍNH";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lblKQ
+            // 
+            lblKQ.AutoSize = true;
+            lblKQ.Location = new Point(204, 187);
+            lblKQ.Name = "lblKQ";
+            lblKQ.Size = new Size(110, 33);
+            lblKQ.TabIndex = 3;
+            lblKQ.Text = "Kết Quả";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(479, 315);
+            button2.Name = "button2";
+            button2.Size = new Size(148, 47);
+            button2.TabIndex = 2;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 33F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(667, 390);
+            Controls.Add(lblKQ);
+            Controls.Add(lbl);
+            Controls.Add(button2);
+            Controls.Add(btxDIENTICH);
+            Controls.Add(button1);
+            Controls.Add(btxCHUVI);
+            Controls.Add(txtB);
+            Controls.Add(txtA);
+            Controls.Add(lblB);
+            Controls.Add(lblA);
+            Font = new Font("Tahoma", 20F);
+            Margin = new Padding(6, 7, 6, 7);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HÌNH CHỮ NHẬT";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblA;
+        private Label lblB;
+        private TextBox txtB;
+        private TextBox txtA;
+        private Button btxCHUVI;
+        private Button btxDIENTICH;
+        private Label lbl;
+        private Button button1;
+        private Label lblKQ;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+namespace CSharp_TinhChuViDienTich
+{
+    public partial class Form1 : Form
+    {
+        double a;
+        double b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (LayThongTin())
+            {               
+                lblKQ.Text = "- Chu Vi: " + TinhChuVi(a, b) + Environment.NewLine + "- Diện tích: " + TinhDienTich(a, b);
+            }
+            else
+            {
+                lblKQ.Text = ""; 
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btxCHUVI_Click(object sender, EventArgs e)
+        {
+            if (LayThongTin())
+            lblKQ.Text = "Chu vi: " + TinhChuVi(a, b);
+            else
+                lblKQ.Text = "";
+        }
+
+        private void btxDIENTICH_Click(object sender, EventArgs e)
+        {
+            LayThongTin();
+            lblKQ.Text = "Diện tích: " + TinhDienTich(a, b);
+        }
+        private double TinhChuVi(double d, double r)
+        {
+            return (d + r) * 2;
+        }
+        private double TinhDienTich(double d, double r)
+        {
+            return (d * r);
+        }
+        private bool LayThongTin()
+        {
+            try
+            {
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Không được nhập chữ, Phải nhập số", "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtA.Clear();
+                txtB.Clear();
+                txtA.Focus();
+                return false;
+            }
+        }
+    }
+}
